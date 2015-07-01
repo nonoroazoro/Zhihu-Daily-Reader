517a99897f85a4315058c08907994fd67dccb0f7
nonoroazoro <xiaochao.k@gmail.com>
2015-07-01 14:43:17 +0800

    update structure

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ var path = require('path');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
 // favicon setup
-app.use(favicon(path.join(__dirname, 'public/common/images/favicon.ico')));
+app.use(favicon(path.join(__dirname, 'app/common/images/favicon.ico')));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -23,12 +19,8 @@ app.use(bodyParser.urlencoded(
 }));
 app.use(cookieParser());
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./config/routes');
 app.use('/', routes);
-app.use('/users', users);
-
-// app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next)
@@ -40,10 +32,10 @@ app.use(function (req, res, next)
 
 // error handlers
 var resolver = require('./lib/path-resolver')(path.join(__dirname,
-  'public/common'));
+  'app/common'));
 
-// development error handler
-// will print stacktrace
+// development error handler will print stacktrace
+// production error handler no stacktraces leaked to user
 if (app.get('env') === 'development')
 {
   app.use(function (err, req, res, next)
@@ -59,21 +51,21 @@ if (app.get('env') === 'development')
     res.end();
   });
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next)
+else
 {
-  res.status(err.status || 500);
-
-  var fn = jade.compileFile(resolver.resolve('error.jade'));
-  res.write(fn(
+  app.use(function (err, req, res, next)
   {
-    message: err.message,
-    error:
-    {}
-  }));
-  res.end();
-});
+    res.status(err.status || 500);
+
+    var fn = jade.compileFile(resolver.resolve('error.jade'));
+    res.write(fn(
+    {
+      message: err.message,
+      error:
+      {}
+    }));
+    res.end();
+  });
+}
 
 module.exports = app;
--- config/routes.js
+++ config/routes.js
@@ -4,9 +4,9 @@ var path = require('path');
 var router = express.Router();
 
 var resolver = require('../lib/path-resolver')(path.join(__dirname,
-  '../public/home'));
+  '../app/home'));
 
-/* GET home page. */
+// home page
 router.get('/', function (req, res, next)
 {
   var fn = jade.compileFile(resolver.resolve('index.jade'));
