2b0261590dc79cab53fe94cbe943afb505557c51
nonoroazoro <xiaochao.k@gmail.com>
2015-07-06 16:00:39 +0800

    change codebase structure

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,7 @@ var router = express.Router();
 // home page
 router.get("/", function (req, res, next)
 {
-    res.render("index",
-    {
-        title: "Express"
-    });
+    res.render("index");
 });
 
 router.use("/api/0", require("./api/0"));
