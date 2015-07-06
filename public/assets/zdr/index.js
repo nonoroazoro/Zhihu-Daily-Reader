698dfb38bc7741ba9d876a0d749baca88bb187e7
nonoroazoro <xiaochao.k@gmail.com>
2015-07-06 17:18:20 +0800

    update home page

--- public/assets/zdr/index.js
+++ public/assets/zdr/index.js
@@ -1,1 +1,13 @@
-﻿
+﻿var Header = React.createClass(
+    {
+        render: function ()
+        {
+            return (<header><h1>Hello</h1></header>);
+        }
+    }
+);
+
+React.render(
+    <Header />,
+    document.body
+);
