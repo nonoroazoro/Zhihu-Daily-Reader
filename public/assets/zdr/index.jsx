1a328dec2d7e37fde5718dd5487275d00842f61d
nonoroazoro <xiaochao.k@gmail.com>
2015-07-08 19:29:38 +0800

    change to .jsx

---
+++ public/assets/zdr/index.jsx
@@ -0,0 +1,18 @@
+﻿define(function (require)
+{
+    var React = require("react");
+    var $ = require("jquery");
+    var Header = React.createClass(
+        {
+            render: function ()
+            {
+                return (<header><h1>Hello</h1></header>);
+            }
+        }
+    );
+    
+    React.render(
+        <Header />,
+        $(".container-fluid")[0]
+    );
+});
