f009d5d339f814d66d29f6ace5b57dbebd875846
nonoroazoro <xiaochao.k@gmail.com>
2015-07-08 17:51:22 +0800

    update for jsx

--- public/assets/zdr/config.js
+++ public/assets/zdr/config.js
@@ -3,7 +3,14 @@ require.config(
     {
         baseUrl: "assets/zdr",
         paths: {
-            jquery: "../libs/jquery/jquery.min"
+            jquery: "../libs/jquery/jquery.min",
+            react: "../libs/react/react.min",
+            JSXTransformer: "../libs/react/JSXTransformer",
+            jsx: "../libs/require/plugins/jsx",
+            text: "../libs/require/plugins/text",
+        },
+        jsx: {
+            fileExtension: ".jsx"
         }
     }
 );
