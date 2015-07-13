a5f025bb350d469f0c90d319f1b79fc650ef2695
nonoroazoro <xiaochao.k@gmail.com>
2015-07-13 16:04:13 +0800

    add navbar menu

--- public/assets/zdr/components/Navbar.jsx
+++ public/assets/zdr/components/Navbar.jsx
@@ -3,23 +3,36 @@ var Navbar = React.createClass(
     {
         render: function ()
         {
-            var navBrandStyle = {height: "73px"};
-            var navButtonStyle = {top: "12px"};
-            var brand = require("common/res/img/zhihu_daily_logo.png");
+            var navBrandStyle = {
+                "padding-top": "6px",
+                "padding-bottom": "6px"
+            };
+            var navBrandImg = require("common/res/img/zhihu_daily_logo.png");
 
-            var nav = 
+            var nav =
                 <nav className="navbar navbar-default navbar-fixed-top" >
                     <div className="container">
                         <div className="navbar-header">
-                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" style={navButtonStyle}>
+                            <button type="button"
+                                className="navbar-toggle collapsed"
+                                data-toggle="collapse"
+                                data-target="#navbarContent">
                                 <span className="sr-only">导航</span>
                                 <span className="icon-bar"></span>
                                 <span className="icon-bar"></span>
                                 <span className="icon-bar"></span>
                             </button>
-                            <a className="navbar-brand" href="#" style={navBrandStyle}>
-                                <img alt="知乎日报" src={brand} />
+                            <a className="navbar-brand" href="/" style={navBrandStyle}>
+                                <img alt="知乎日报" src={navBrandImg} />
                             </a>
+                        </div>
+
+                        <div id="navbarContent" className="navbar-collapse collapse" >
+                            <ul className="nav navbar-nav navbar-right">
+                                <li className="active"><a href="#">日报</a></li>
+                                <li><a href="#">专栏</a></li>
+                                <li><a href="#">关于</a></li>
+                            </ul>
                         </div>
                     </div>
                 </nav>;
