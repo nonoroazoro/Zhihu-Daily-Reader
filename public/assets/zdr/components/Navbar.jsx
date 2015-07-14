dc96a99a242f1b443173d385324a887224e22452
nonoroazoro <xiaochao.k@gmail.com>
2015-07-14 19:03:59 +0800

    add collapse id

--- public/assets/zdr/components/Navbar.jsx
+++ public/assets/zdr/components/Navbar.jsx
@@ -1,11 +1,13 @@
 ﻿require("./res/Navbar.less");
 
+var _ = require("lodash");
 var React = require("react");
 var Navbar = React.createClass(
     {
         render: function ()
         {
             const logo = require("common/res/img/zhihu_daily_logo.png");
+            const collapseId = _.uniqueId("collapseContent");
 
             var nav =
                 <nav className="Navbar navbar navbar-default navbar-fixed-top" >
@@ -14,7 +16,7 @@ var Navbar = React.createClass(
                             <button type="button"
                                 className="navbar-toggle collapsed"
                                 data-toggle="collapse"
-                                data-target=".Navbar .NavbarCollapse">
+                                data-target={"#" + collapseId}>
                                 <span className="sr-only">导航</span>
                                 <span className="icon-bar"></span>
                                 <span className="icon-bar"></span>
@@ -25,7 +27,7 @@ var Navbar = React.createClass(
                             </a>
                         </div>
 
-                        <div className="NavbarCollapse navbar-collapse collapse" >
+                        <div id={collapseId} className="navbar-collapse collapse" >
                             <ul className="nav navbar-nav navbar-right">
                                 <li className="active"><a href="#">日报</a></li>
                                 <li><a href="#">专栏</a></li>
