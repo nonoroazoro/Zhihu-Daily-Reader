93ad1631df2fac6d4bef62b3fb5aca487a0a1159
nonoroazoro <xiaochao.k@gmail.com>
2015-07-15 16:42:51 +0800

    splitted to comps

--- public/assets/zdr/components/Navbar.jsx
+++ public/assets/zdr/components/Navbar.jsx
@@ -1,45 +1,71 @@
 ﻿require("./res/Navbar.less");
 
-var _ = require("lodash");
 var React = require("react");
-var Navbar = React.createClass(
+
+/**
+ * 头部。
+ */
+var NavbarHeader = React.createClass(
+{
+    render: function ()
     {
-        render: function ()
-        {
-            const logo = require("common/res/img/zhihu_daily_logo.png");
-            const collapseId = _.uniqueId("collapseContent");
-
-            var nav =
-                <nav className="Navbar navbar navbar-default navbar-fixed-top" >
-                    <div className="container">
-                        <div className="navbar-header">
-                            <button type="button"
-                                className="navbar-toggle collapsed"
-                                data-toggle="collapse"
-                                data-target={"#" + collapseId}>
-                                <span className="sr-only">导航</span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                            </button>
-                            <a className="navbar-brand" href="/">
-                                <img alt="知乎日报" src={logo} />
-                            </a>
-                        </div>
-
-                        <div id={collapseId} className="navbar-collapse collapse" >
-                            <ul className="nav navbar-nav navbar-right">
-                                <li className="active"><a href="#">日报</a></li>
-                                <li><a href="#">专栏</a></li>
-                                <li><a href="#">关于</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>;
-
-            return nav;
-        }
+        var navbarHeader =
+            <div className="navbar-header">
+                <button type="button"
+                    className="navbar-toggle collapsed"
+                    data-toggle="collapse"
+                    data-target={this.props.collapseTarget ? this.props.collapseTarget : ".navbar-collapse"}>
+                    <span className="sr-only">导航</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="/">
+                    <img alt="知乎日报" src={require("common/res/img/zhihu_daily_logo.png")} />
+                </a>
+            </div>;
+        return navbarHeader;
     }
-);
+});
+
+/**
+ * 菜单。
+ */
+var NavbarContent = React.createClass(
+{
+    render: function ()
+    {
+        var navbarContent =
+            <div id={this.props.id ? this.props.id : "NavbarContent"}
+                className="navbar-collapse collapse">
+                <ul className="nav navbar-nav navbar-right">
+                    <li className="active"><a href="#">日报</a></li>
+                    <li><a href="#">专栏</a></li>
+                    <li><a href="#">关于</a></li>
+                </ul>
+            </div>;
+        return navbarContent;
+    }
+});
+
+/**
+ * 导航栏。
+ */
+var Navbar = React.createClass(
+{
+    render: function ()
+    {
+
+        var navbar =
+            <nav id={this.props.id ? this.props.id : null}
+                className="Navbar navbar navbar-default navbar-fixed-top">
+                <div className="container">
+                    <NavbarHeader collapseTarget="#NavbarContent"/>
+                    <NavbarContent />
+                </div>
+            </nav>;
+        return navbar;
+    }
+});
 
 module.exports = Navbar;
