fbbbec286777b1a4076d9aaff1b207d9e8a2fc12
nonoroazoro <xiaochao.k@gmail.com>
2015-07-15 16:52:04 +0800

    add jsx harmony

--- public/assets/zdr/webpack.config.js
+++ public/assets/zdr/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.jsx?$/, loader: "jsx" },
+            { test: /\.jsx?$/, loader: "jsx?harmony" },
             { test: /\.css$/, loader: ExtractTextPlugin.extract("style", "css") },
             { test: /\.less$/, loader: ExtractTextPlugin.extract("style", "css!less") },
             { test: /\.(png|jpg)$/, loader: "url?limit=8192" },
