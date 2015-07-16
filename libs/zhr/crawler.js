13523b8e5d21349f41d2e6db193a30af6259717b
nonoroazoro <xiaochao.k@gmail.com>
2015-07-16 17:54:37 +0800

    add zhihu-daily api

---
+++ libs/zhr/crawler.js
@@ -0,0 +1,34 @@
+﻿var http = require("http");
+
+/**
+ * 从知乎日报获取最新消息。
+ */
+function getLatestNews(p_callback)
+{
+    http.get("http://news-at.zhihu.com/api/4/news/latest", function (res)
+    {
+        if (res.statusCode == 200)
+        {
+            var data = "";
+            res.on("data", function (chunk)
+            {
+                data += chunk;
+            }).on("end", function ()
+            {
+                if (p_callback)
+                {
+                    p_callback(JSON.parse(data));
+                }
+            });
+        }
+        else
+        {
+            if (p_callback)
+            {
+                p_callback({});
+            }
+        }
+    });
+}
+
+exports.getLatestNews = getLatestNews;
