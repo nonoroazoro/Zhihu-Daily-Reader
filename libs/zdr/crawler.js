acb4642f6ac4293dc5a3e0f14c929b445b1e71f0
nonoroazoro <xiaochao.k@gmail.com>
2015-07-17 23:07:24 +0800

    update getLatestNews

--- libs/zdr/crawler.js
+++ libs/zdr/crawler.js
@@ -1,14 +1,57 @@
-﻿var options = { baseUrl : "http://news-at.zhihu.com/api/4/" };
+﻿var querystring = require("querystring");
+
+var options = { baseUrl : "http://news-at.zhihu.com/api/4/" };
 var dailyRequest = require("request").defaults(options);
 var imgRequest = require("request");
 
 /**
- * 从知乎日报获取最新消息。
+ * 从知乎日报获取最新消息（包括当日新闻和热点新闻）。
  * @param res 服务端响应。
  */
 function getLatestNews(res)
 {
-    dailyRequest.get("/news/latest").pipe(res);
+    //dailyRequest.get("/news/latest").pipe(res);
+    const prefix = "/api/4/imgs/";
+    dailyRequest.get({ url: "/news/latest", json: true }, function (error, response, body)
+    {
+        if (!error && response.statusCode == 200)
+        {
+            var stories = body.stories;
+            var storiesLength = stories.length;
+            var images = [];
+            var imagesLength = 0;
+            for (var i = 0; i < storiesLength; i++)
+            {
+                delete stories[i].type;
+                delete stories[i].ga_prefix;
+                delete stories[i].multipic;
+                
+                images = stories[i].images;
+                imagesLength = images.length;
+                for (var j = 0; j < imagesLength; j++)
+                {
+                    images[j] = prefix + querystring.escape(images[j]);
+                }
+            }
+            
+            stories = body.top_stories;
+            storiesLength = stories.length;
+            for (var i = 0; i < storiesLength; i++)
+            {
+                delete stories[i].type;
+                delete stories[i].ga_prefix;
+                
+                stories[i].image = prefix + querystring.escape(stories[i].image);
+            }
+            
+            res.set(response.headers);
+            res.json(body);
+        }
+        else
+        {
+            res.send({ error: "Document not found" });
+        }
+    });
 }
 
 /**
