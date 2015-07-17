b6b70fadfeaa988e41b5b0bfa249832b425498a6
nonoroazoro <xiaochao.k@gmail.com>
2015-07-17 18:04:53 +0800

    rename zhr to zdr

---
+++ libs/zdr/crawler.js
@@ -0,0 +1,25 @@
+﻿var options = { baseUrl : "http://news-at.zhihu.com/api/4/" };
+var dailyRequest = require("request").defaults(options);
+var imgRequest = require("request");
+
+/**
+ * 从知乎日报获取最新消息。
+ * @param res 服务端响应。
+ */
+function getLatestNews(res)
+{
+    dailyRequest.get("/news/latest").pipe(res);
+}
+
+/**
+ * 从知乎日报获取指定图片。
+ * @param url 图片地址。
+ * @param res 服务端响应。
+ */
+function getImage(url, res)
+{
+    imgRequest.get(url).pipe(res);
+}
+
+exports.getLatestNews = getLatestNews;
+exports.getImage = getImage;
