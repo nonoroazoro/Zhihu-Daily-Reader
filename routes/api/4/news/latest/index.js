760701333329bfaaff4a82b9ab8aa6e13dc6b369
nonoroazoro <xiaochao.k@gmail.com>
2015-07-17 18:04:12 +0800

    update

--- routes/api/4/news/latest/index.js
+++ routes/api/4/news/latest/index.js
@@ -1,12 +1,12 @@
 ﻿var express = require("express");
 var router = express.Router();
 
-var crawler = require(__base + "/libs/zhr/crawler");
+var crawler = require(__base + "/libs/zdr/crawler");
 
 // get latest news
 router.get("/", function (req, res, next)
 {
-    // 暂时先用这种方法，后面加上爬虫。读取本地缓存。
+    //TODO: 暂时先用这种方法，后面加上爬虫。读取本地缓存。
     crawler.getLatestNews(res);
 });
 
