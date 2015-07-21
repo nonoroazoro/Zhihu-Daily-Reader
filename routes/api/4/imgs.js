bb68014800e86919790e189dcdd453ff16d36835
nonoroazoro <xiaochao.k@gmail.com>
2015-07-21 16:45:25 +0800

    update comments

--- routes/api/4/imgs.js
+++ routes/api/4/imgs.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 var crawler = require(__base + "/libs/zdr/crawler");
 
-// get zhihu daily image
+// get zhihu daily image.
 router.get("/:url", function (req, res, next)
 {
     //TODO: 暂时先用这种方法，后面加上爬虫。读取本地缓存。
