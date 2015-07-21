111c7b3b4c2ac62202bc2edf15a2593c204bd72f
nonoroazoro <xiaochao.k@gmail.com>
2015-07-21 17:22:59 +0800

    add FlexView

---
+++ public/assets/zdr/components/FlexView.jsx
@@ -0,0 +1,23 @@
+﻿require("./res/FlexView.less");
+
+var React = require("react");
+
+var FlexView = React.createClass(
+{
+        render: function ()
+        {
+            var items = [];
+            for (var i = 0; i < 12; i++)
+            {
+                items.push(<div className="flex-tile" id={"tile" + i} />);
+            }
+
+            return (
+                <div className="FlexView">
+                    {items}
+                </div>
+            );
+        }
+});
+
+module.exports = FlexView;
