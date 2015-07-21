366e9fc1cbda93614891ed6dca7c5e4f1e38701f
nonoroazoro <xiaochao.k@gmail.com>
2015-07-21 17:17:15 +0800

    add some apis

--- routes/api/4/news/index.js
+++ routes/api/4/news/index.js
@@ -16,9 +16,11 @@ apis.forEach(function (p_api)
 router.get("/:id", function (req, res, next)
 {
     // TODO: 获取指定 id 的日报。
-    res.send({
-        haha: "这是你妹:" + req.params.id
-    });
+    res.send(
+        {
+            haha: "这是你妹:" + req.params.id
+        }
+    );
 });
 
 module.exports = router;
