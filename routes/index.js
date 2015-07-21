be7d2ddbc7c456cb78de071d4d6aea68be78cd4e
nonoroazoro <xiaochao.k@gmail.com>
2015-07-21 16:25:05 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,7 @@
 ﻿var express = require("express");
 var router = express.Router();
 
-// home page
-router.get("/", function (req, res, next)
-{
-    res.render("index");
-});
-
+// 指定的 API 需优先判断。
 var apis = [
     "/api/4",
 ];
@@ -14,6 +9,12 @@ var apis = [
 apis.forEach(function (p_api)
 {
     router.use(p_api, require("." + p_api));
+});
+
+// home page.
+router.get("/", function (req, res, next)
+{
+    res.render("index");
 });
 
 module.exports = router;
