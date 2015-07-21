fa0542898516547519d43e635b676d8493a03e09
nonoroazoro <xiaochao.k@gmail.com>
2015-07-21 16:34:18 +0800

    add some api

--- routes/api/4/news/index.js
+++ routes/api/4/news/index.js
@@ -2,12 +2,23 @@
 var router = express.Router();
 
 var apis = [
-    "/latest"
+    "/before",
+    "/latest",
+    "/top",
 ];
 
 apis.forEach(function (p_api)
 {
     router.use(p_api, require("." + p_api));
+});
+
+// get specified story.
+router.get("/:id", function (req, res, next)
+{
+    // TODO: 获取指定 id 的日报。
+    res.send({
+        haha: "这是你妹:" + req.params.id
+    });
 });
 
 module.exports = router;
