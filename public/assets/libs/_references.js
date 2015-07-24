8bd23d71240c76d5d5d510e0e8595154e3896bc9
nonoroazoro <xiaochao.k@gmail.com>
2015-07-24 16:35:36 +0800

    remove

--- public/assets/libs/_references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="jquery/jquery.js" />
-/// <reference path="react/react.js" />
-/// <reference path="bootstrap/js/bootstrap.js" />
-/// <reference path="lodash/lodash.js" />
