664b7df86294f21612ef19fc46e3a73100df202b
nonoroazoro <xiaochao.k@gmail.com>
2015-07-25 18:17:49 +0800

    add controllers

--- public/assets/zdr/daily/DailyPage.jsx
+++ public/assets/zdr/daily/DailyPage.jsx
@@ -1,19 +1,44 @@
 ﻿require("./res/DailyPage.less");
 
 var React = require("react");
+var DailyManager = require("./controllers/DailyManager");
 
 var Carousel = require("./components/Carousel");
 var FlexView = require("./components/FlexView");
 var ArticleView = require("./components/ArticleView");
 
+/**
+ * 知乎日报页面。
+ */
 var DailyPage = React.createClass(
 {
+    getInitialState()
+    {
+        return {
+            topStories: []
+        };
+    },
+
+    componentDidMount: function ()
+    {
+        DailyManager.getTopStories(function (data)
+        {
+            if (data && this.isMounted())
+            {
+                this.setState(
+                {
+                    topStories: data.stories
+                });
+            }
+        }.bind(this));
+    },
+
     render : function()
     {
         var page =
             <div className="DailyPage container-fluid">
                 <div className="CarouselContainer container-fluid">
-                <Carousel />
+                <Carousel data={this.state.topStories}/>
                 </div>
                 <div className="FlexContainer container-fluid">
                     <FlexView />
---
+++ public/assets/zdr/daily/controllers/DailyManager.js
@@ -0,0 +1,11 @@
+﻿var $ = require("jquery");
+
+/**
+ * 获取最新热门日报。
+ */
+function getTopStories(callback)
+{
+    $.get("/api/4/news/top", callback);
+}
+
+module.exports.getTopStories = getTopStories;
