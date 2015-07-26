cc40155239025a3d14de54ebd8846df5b6573d8e
nonoroazoro <xiaochao.k@gmail.com>
2015-07-26 15:15:14 +0800

    add getStories & getStory functions

--- public/assets/zdr/daily/controllers/DailyManager.js
+++ public/assets/zdr/daily/controllers/DailyManager.js
@@ -8,4 +8,25 @@ function getTopStories(callback)
     $.get("/api/4/news/top", callback);
 }
 
+/**
+ * 获取指定日期的日报。
+ * @param String p_date 指定的日期。如果小于 20130519，返回值为 {}。
+ */
+function getStories(callback, p_date)
+{
+    var url = (p_date == null || p_date == "") ? "/api/4/news/before" : "/api/4/news/before/" + p_date;
+    $.get(url, callback);
+}
+
+/**
+ * 获取指定唯一标识的日报。
+ * @param String p_id 指定的唯一标识。
+ */
+function getStory(callback, p_id)
+{
+    $.get("/api/4/news/" + p_id, callback);
+}
+
 module.exports.getTopStories = getTopStories;
+module.exports.getStories = getStories;
+module.exports.getStory = getStory;
