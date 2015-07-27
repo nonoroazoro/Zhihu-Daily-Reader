e891f278d8735f95f6c65da90307bff9e69dffcf
nonoroazoro <xiaochao.k@gmail.com>
2015-07-27 21:44:34 +0800

    remove DEBUG code

--- public/assets/zdr/daily/components/ArticleView.jsx
+++ public/assets/zdr/daily/components/ArticleView.jsx
@@ -50,45 +50,26 @@ var ArticleView = React.createClass(
         };
     },
 
-    // 测试数据
-    getInitialState: function ()
-    {
-        return {
-            story: null
-        };
-    },
-
-    // 测试数据
-    componentDidMount: function ()
-    {
-        $.get("http://127.0.0.1:3000/api/4/news/3892357", function (data)
-        {
-            if (this.isMounted() && data)
-            {
-                this.setState(
-                {
-                    story: data
-                });
-            }
-        }.bind(this)).fail(function ()
-        {
-            console.log("error loading top story");
-        });
-    },
-
     render: function ()
     {
+        // 无内容时隐藏 ArticleView。
+        var classes = classNames(
+            "ArticleView", "modal", "fade", "in",
+            {
+                "hide": (!this.props.story),
+            }
+        );
+
         var articleView =
-            <div id={this.props.id} className="ArticleView modal fade in">
+            <div id={this.props.id} className={classes}>
                 <div className="modal-dialog">
                     <div className="modal-content">
-                        <ArticleHeader data={this.state.story} />
-                        <ArticleBody data={this.state.story} />
-                        <ArticleFooter data={this.state.story} />
+                        <ArticleHeader data={this.props.story} />
+                        <ArticleBody data={this.props.story} />
+                        <ArticleFooter data={this.props.story} />
                     </div>
                 </div>
             </div>;
-            
         return articleView;
     }
 });
