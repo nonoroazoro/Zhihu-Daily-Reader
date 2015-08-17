c30bf6ba055efc9ce2d07556b4452b31e399731b
nonoroazoro <xiaochao.k@gmail.com>
2015-08-17 15:48:13 +0800

    add Preloader

---
+++ public/assets/zdr/daily/components/Preloader.jsx
@@ -0,0 +1,36 @@
+﻿require("./res/Preloader.less");
+
+var _ = require("lodash");
+var React = require("react");
+var classNames = require("classnames");
+
+var Preloader = React.createClass(
+{
+    getDefaultProps: function ()
+    {
+        return {
+            className: null
+        };
+    },
+
+    render: function ()
+    {
+        var classes = "Preloader";
+        if(!_.isEmpty(this.props.className))
+        {
+            classes = classes + " " + this.props.className
+        }
+        
+        return (
+            <div className={classes}>
+                <span className="wave1"/>
+                <span className="wave2"/>
+                <span className="wave3"/>
+                <span className="wave4"/>
+                <span className="wave5"/>
+            </div>
+        );
+    }
+});
+
+module.exports = Preloader;
