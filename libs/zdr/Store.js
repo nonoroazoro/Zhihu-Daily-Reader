3931e114349d2158e0a67abfaab1d8beb47556ac
nonoroazoro <xiaochao.k@gmail.com>
2015-08-26 15:02:41 +0800

    add test code

---
+++ libs/zdr/Store.js
@@ -0,0 +1,53 @@
+﻿var mongoose = require("mongoose");
+
+/**
+ * 测试。
+ */
+function test()
+{
+    mongoose.connect("mongodb://localhost/zhihu");
+    
+    var db = mongoose.connection;
+    db.on("error", function callback()
+    {
+        console.log("Connection error");
+    });
+    
+    db.once("open", function callback()
+    {
+        console.log("Mongo working!");
+    });
+    
+    
+    var Schema = mongoose.Schema;
+    var userSchema = new Schema({
+        name : String,
+        age : Number,
+        DOB : Date,
+        isAlive : Boolean
+    });
+    
+    var User = mongoose.model("User", userSchema);
+    
+    var newUser = new User({
+        name : "Arvind",
+        age : 99,
+        DOB : "01/01/1915",
+        isAlive : true
+    });
+    
+    newUser.save(function (err, data)
+    {
+        if (err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("Saved : ", data);
+        }
+    });
+
+}
+
+exports.test = test;
