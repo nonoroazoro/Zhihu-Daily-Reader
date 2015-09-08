eb71b54e6d31686177eae209e5f4f964669dad28
nonoroazoro <xiaochao.k@gmail.com>
2015-09-08 14:41:18 +0800

    add Models

---
+++ models/index.js
@@ -0,0 +1,19 @@
+﻿var mongoose = require("mongoose");
+var config = require("config");
+
+var options = {
+    server: {
+        auto_reconnect: config.autoReconnect,
+        poolSize: config.poolSize
+    },
+};
+
+mongoose.connect(config.db, options, function (err)
+{
+    if (err)
+    {
+        console.error("Failed connecting to %s: ", config.db, err.message);
+    }
+});
+
+exports.Story = require("./story");
