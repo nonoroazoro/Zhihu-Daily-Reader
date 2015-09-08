290a9f5d027769d0f83bd67cabc766a5e078fa65
nonoroazoro <xiaochao.k@gmail.com>
2015-09-08 14:39:30 +0800

    update log

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,5 @@
-﻿#!/usr/bin/env node
+﻿var config = require("config");
+require("../app").listen(config.port);
 
-var config = require("config");
-var port = process.env.PORT || config.port;
-require("../app").listen(port);
-
-console.log("Server Started on Port: " + port);
+console.log("Server started on port: " + config.port);
+console.log("\n(Change it in file: ./config/production.json)");
