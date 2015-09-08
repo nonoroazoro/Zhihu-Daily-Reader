2e692b26425296fa70b28e8d635255ab270bfabf
nonoroazoro <xiaochao.k@gmail.com>
2015-09-08 14:41:07 +0800

    add Model: Story

---
+++ models/story.js
@@ -0,0 +1,11 @@
+﻿var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var StorySchema = new Schema({
+    content: { type: String },
+    id: { type: String },
+    date: { type: Date },
+    read: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model("Story", StorySchema);
