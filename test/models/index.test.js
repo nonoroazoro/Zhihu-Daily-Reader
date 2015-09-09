2c239c65a061a7acf1c79e28df2e3ad2d5eb8a93
nonoroazoro <xiaochao.k@gmail.com>
2015-09-09 13:05:56 +0800

    add test for models

---
+++ test/models/index.test.js
@@ -0,0 +1,59 @@
+﻿require("should");
+require("../../models");
+
+var _ = require("lodash");
+var mongoose = require("mongoose")
+var Story = require("../../models/story");
+
+describe("Models", function ()
+{
+    before(function (done)
+    {
+        var db = mongoose.connection;
+        if (db.readyState === 0)
+        {
+            done(new Error("Mongodb not Connected"));
+        }
+        else
+        {
+            _.each(db.collections, function (collection, name)
+            {
+                collection.drop(function (err)
+                {
+                    if (err)
+                    {
+                        done(err)
+                    }
+                });
+            });
+            
+            done();
+        }
+    });
+    
+    describe("Story#create", function ()
+    {
+        it("should create a new story: 7104770", function (done)
+        {
+            new Story({
+                id : "7104770",
+                date : "20150909",
+                read : false,
+                content : "神经科学/生物学",
+            }).save(done);
+        });
+    });
+    
+    describe("Story#find", function ()
+    {
+        it("should find the story: 7104770", function (done)
+        {
+            Story.find({ id: "7104770" }, function (err, story)
+            {
+                should.not.exist(err);
+                story.id.should.equal("7104770");
+                done();
+            });
+        });
+    });
+});
