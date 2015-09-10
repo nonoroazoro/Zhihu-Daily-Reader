055aefa82fe1bebf0ea8c0ed9161879bf91cf96a
nonoroazoro <xiaochao.k@gmail.com>
2015-09-10 16:34:48 +0800

    add mongodb connect test

---
+++ test/models/connect.test.js
@@ -0,0 +1,38 @@
+﻿var mongoose = require("mongoose");
+
+describe("Mongodb Connection", function ()
+{
+    describe("Connect", function ()
+    {
+        it("should create a new UserSchema", function (done)
+        {
+            var Schema = mongoose.Schema;
+            var userSchema = new Schema({
+                name : String,
+                age : Number,
+                DOB : Date,
+                isAlive : Boolean
+            });
+            mongoose.model("User", userSchema);
+            done();
+        });
+        
+        it("should create a new User", function (done)
+        {
+            var User = mongoose.model("User");
+            var newUser = new User({
+                name : "Arvind",
+                age : 99,
+                DOB : "01/01/1915",
+                isAlive : true
+            });
+            newUser.save(done);
+        });
+    });
+    
+    after(function ()
+    {
+        var User = mongoose.model("User");
+        User.findOneAndRemove({ name: "Arvind" });
+    });
+});
