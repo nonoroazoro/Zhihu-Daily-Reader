51555f95e8a8d1d779d9141c2690616227fc82f7
nonoroazoro <xiaochao.k@gmail.com>
2015-09-14 17:49:37 +0800

    add utils, crawler and update

--- controllers/crawler.js
+++ controllers/crawler.js
@@ -1,8 +1,65 @@
-﻿var config = require("config");
+﻿var _ = require("lodash");
+var async = require("async");
+var config = require("config");
 var moment = require("moment");
-var cheerio = require("cheerio");
-var querystring = require("querystring");
+var mongoose = require("mongoose");
 
-var options = { baseUrl : config.zhihu_daily_api };
-var dailyRequest = require("request").defaults(options);
-var imgRequest = require("request");
+var utils = require("./utils");
+var database = require("./database");
+var Status = require("../models/status");
+
+exports.fetchStories = function (p_callback)
+{
+}
+
+function _saveStories()
+{
+}
+
+/**
+ * 启动爬虫。
+ */
+exports.run = function ()
+{
+    if (database.connected())
+    {
+        async.waterfall(
+            [
+                _initTask,
+            ], function (err, result)
+            {
+                console.log(err);
+                console.log(result);
+            }
+        );
+    }
+}
+
+/**
+ * 初始化，得到爬虫起始日期，例如："20150915"。
+ */
+function _initTask(done)
+{
+    Status.findOne({ username: config.username }, function (err, res)
+    {
+        var date = null;
+        if (!err && res)
+        {
+            date = res.oldest;
+        }
+        
+        if (!date)
+        {
+            date = utils.convertToZhihuDate(Date.now());
+        }
+        
+        done(null, date);
+    });
+}
+
+/**
+ * 爬取指定日期的日报。
+ */
+function _fetchStoriesTask(p_date, done)
+{
+}
