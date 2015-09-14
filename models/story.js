0471c0bc53d8a21082c9b0609917011d051691c2
nonoroazoro <xiaochao.k@gmail.com>
2015-09-14 13:40:06 +0800

    add comment

--- models/story.js
+++ models/story.js
@@ -1,10 +1,32 @@
 ﻿var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+/**
+ * 存储知乎日报内容。
+ */
 var StorySchema = new Schema({
+    /**
+     * Id。
+     * @type {String}
+     */
     id: { type: String },
+    
+    /**
+     * 日期。
+     * @type {String}
+     */
     date: { type: String },
+    
+    /**
+     * 已读状态。
+     * @type {Boolean}
+     */
     read: { type: Boolean, default: false },
+    
+    /**
+     * 离线日报内容。
+     * @type {String}
+     */
     content: { type: String },
 });
 
