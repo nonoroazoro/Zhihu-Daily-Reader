602929f8ed71b46cf9dd8c058f803cc82de18cab
nonoroazoro <xiaochao.k@gmail.com>
2015-09-14 15:49:17 +0800

    rename crawler to proxy

--- routes/api/4/news/before.js
+++ routes/api/4/news/before.js
@@ -1,7 +1,7 @@
 ﻿var express = require("express");
 var router = express.Router();
 
-var crawler = require(__base + "/controllers/crawler");
+var proxy = require(__base + "/controllers/proxy");
 
 // get stroies of the specified date.
 router.get("/:date?", function (req, res, next)
@@ -9,12 +9,12 @@ router.get("/:date?", function (req, res, next)
     if (req.params.date)
     {
         //TODO: 暂时先用这种方法，后面加上爬虫。读取本地缓存。
-        crawler.getStoryIndexes(req.params.date, res);
+        proxy.getStoryIndexes(req.params.date, res);
     }
     else
     {
         // 如果未指定，则返回最新日报的索引。
-        crawler.getLatestStoryIndexes(res);
+        proxy.getLatestStoryIndexes(res);
     }
 });
 
--- routes/api/4/news/top.js
+++ routes/api/4/news/top.js
@@ -1,13 +1,13 @@
 ﻿var express = require("express");
 var router = express.Router();
 
-var crawler = require(__base + "/controllers/crawler");
+var proxy = require(__base + "/controllers/proxy");
 
 // get top stroies.
 router.get("/", function (req, res, next)
 {
     //TODO: 暂时先用这种方法，后面加上爬虫。读取本地缓存。
-    crawler.getTopStoryIndexes(res);
+    proxy.getTopStoryIndexes(res);
 });
 
 module.exports = router;
