b6d97ebd88585c450fbab037c6af573e01c6bec6
nonoroazoro <xiaochao.k@gmail.com>
2015-09-14 15:49:54 +0800

    add crawler.js

---
+++ controllers/crawler.js
@@ -0,0 +1,8 @@
+﻿var config = require("config");
+var moment = require("moment");
+var cheerio = require("cheerio");
+var querystring = require("querystring");
+
+var options = { baseUrl : config.zhihu_daily_api };
+var dailyRequest = require("request").defaults(options);
+var imgRequest = require("request");
