ee258ab767bff216ffd65dce2ea34f1b64c36c91
nonoroazoro <xiaochao.k@gmail.com>
2015-09-14 15:06:25 +0800

    add status model

---
+++ models/status.js
@@ -0,0 +1,29 @@
+﻿var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+/**
+ * 存储知乎日报阅读器状态（与用户相关）。
+ */
+var StatusSchema = new Schema({
+    /**
+     * 用户名（唯一）。
+     * @type {String}
+     */
+    username: { type: String },
+    
+    /**
+     * 存储的最新日报的日期。
+     * @type {String}
+     */
+    newest: { type: String },
+    
+    /**
+     * 存储的最旧日报的日期。
+     * @type {String}
+     */
+    oldest: { type: String }
+});
+
+StatusSchema.index({ username: 1 }, { unique: true });
+
+module.exports = mongoose.model("Status", StatusSchema);
