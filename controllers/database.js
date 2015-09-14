bc70be373857d55960bfe70cb9ed4c9bd0467072
nonoroazoro <xiaochao.k@gmail.com>
2015-09-14 15:09:44 +0800

    add database controller

---
+++ controllers/database.js
@@ -0,0 +1,56 @@
+﻿var _ = require("lodash");
+var async = require("async");
+var config = require("config");
+var mongoose = require("mongoose");
+
+/**
+ * 连接数据库。
+ * @param  {Function} p_callback 回调函数。
+ */
+exports.connect = function (p_callback)
+{
+    mongoose.connect(
+        config.db,
+        {
+            server: {
+                auto_reconnect: config.auto_reconnect,
+                poolSize: config.poolSize
+            },
+        },
+        p_callback
+    );
+}
+
+/**
+ * 检查数据库是否已连接。
+ */
+ exports.connected = function ()
+{
+    return mongoose.connection.db != null;
+}
+
+/**
+ * 删除所有集合。
+ * @param  {Function} p_callback 回调函数。
+ */
+ exports.dropAllCollections = function (p_callback)
+{
+    async.each(mongoose.connection.collections, function (collection, callback)
+    {
+        collection.drop(callback);
+    },
+    function (err)
+    {
+        if (_.isFunction(p_callback))
+        {
+            if (!err || err.message == "ns not found")
+            {
+                p_callback()
+            }
+            else
+            {
+                p_callback(err);
+            }
+        }
+    });
+}
