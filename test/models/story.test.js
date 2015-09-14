cbb593303df558befb10d319140d1a0d128c3fb6
nonoroazoro <xiaochao.k@gmail.com>
2015-09-14 13:48:17 +0800

    rename

---
+++ test/models/story.test.js
@@ -0,0 +1,60 @@
+﻿var async = require("async");
+var should = require("should");
+var mongoose = require("mongoose");
+
+var models = require("../../models");
+var Story = models.Story;
+
+describe("models/story", function ()
+{
+    before(function (done)
+    {
+        if (models.connected())
+        {
+            _clearDB(done);
+        }
+        else
+        {
+            models.connectDB(function (err)
+            {
+                should.not.exist(err);
+                _clearDB(done);
+            });
+        }
+    });
+    
+    describe("#create", function ()
+    {
+        it("should create a new story: 7104770", function (done)
+        {
+            new Story({
+                id : "7104770",
+                date : "20150909",
+                read : false,
+                content : "神经科学/生物学",
+            }).save(done);
+        });
+    });
+    
+    describe("#find", function ()
+    {
+        it("should find the story: 7104770", function (done)
+        {
+            Story.findOne({ id: "7104770" }, function (err, res)
+            {
+                should.not.exist(err);
+                res.id.should.equal("7104770");
+                done();
+            });
+        });
+    });
+});
+
+function _clearDB(p_done)
+{
+    async.each(mongoose.connection.collections, function (collection, callback)
+    {
+        collection.drop(callback);
+    },
+    p_done);
+}
