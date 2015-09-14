0f6df0c2ff44aa4df05c4aa5168c2937e2806d47
nonoroazoro <xiaochao.k@gmail.com>
2015-09-14 17:11:19 +0800

    add utils test

---
+++ test/controllers/utils.test.js
@@ -0,0 +1,28 @@
+﻿var moment = require("moment");
+var should = require("should");
+
+var utils = require("../../controllers/utils");
+
+describe("controllers/utils", function ()
+{
+    describe("#convertToZhihuDate", function ()
+    {
+        var origin = "2015-09-15";
+        var target = "20150915";
+        var date1 = moment(origin); // String
+        var date2 = moment(origin).toDate(); // Date
+        it("should convert '" + origin + "' to: '" + target + "'", function (done)
+        {
+            utils.convertToZhihuDate(date1).should.equal(target);
+            utils.convertToZhihuDate(date2).should.equal(target);
+            done();
+        });
+        
+        origin = "2015-30-30";
+        it("should convert '" + origin + "' to: 'null'", function (done)
+        {
+            should(utils.convertToZhihuDate(origin)).equal(null);
+            done();
+        });
+    });
+});
