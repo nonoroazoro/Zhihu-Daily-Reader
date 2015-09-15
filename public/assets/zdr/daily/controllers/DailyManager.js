0b7ea8433502a1fa1cce4115ac515dc501bb60f2
nonoroazoro <xiaochao.k@gmail.com>
2015-09-15 10:13:02 +0800

    update

--- public/assets/zdr/daily/controllers/DailyManager.js
+++ public/assets/zdr/daily/controllers/DailyManager.js
@@ -6,7 +6,7 @@ var _stories = {};
 /**
  * 获取目前已从服务端获取到的所有日报内容的缓存（以日报 id 进行检索，无序，请勿用 index 检索）。
  */
-function getFetchedStories()
+exports.getFetchedStories = function ()
 {
     return _stories;
 }
@@ -14,7 +14,7 @@ function getFetchedStories()
 /**
  * 获取最新热门日报的索引。
  */
-function getTopStoryIndexes(callback)
+exports.getTopStoryIndexes = function (callback)
 {
     $.get("/api/4/news/top", function (p_data)
     {
@@ -29,7 +29,7 @@ function getTopStoryIndexes(callback)
  * 获取指定日期的日报的索引。
  * @param String p_date 指定的日期。如果未指定，则返回最新日报的索引；如果小于 20130519，则返回 {}。
  */
-function getStoryIndexes(callback, p_date)
+exports.getStoryIndexes = function (callback, p_date)
 {
     if (_.isEmpty(p_date))
     {
@@ -54,7 +54,7 @@ function getStoryIndexes(callback, p_date)
  * 获取指定唯一标识的日报。
  * @param String p_id 指定的唯一标识。
  */
-function getStory(callback, p_id)
+exports.getStory = function (callback, p_id)
 {
     $.get("/api/4/news/" + p_id, function (p_data)
     {
@@ -65,9 +65,3 @@ function getStory(callback, p_id)
         callback({ error: "error" });
     });
 }
-
-module.exports.getTopStoryIndexes = getTopStoryIndexes;
-module.exports.getStoryIndexes = getStoryIndexes;
-module.exports.getStory = getStory;
-
-module.exports.getFetchedStories = getFetchedStories;
