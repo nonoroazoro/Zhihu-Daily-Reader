951ad65c92ccc5fb730d985d8bf6c128bf4e3a2f
nonoroazoro <xiaochao.k@gmail.com>
2015-09-16 15:49:28 +0800

    update

--- test/controllers/crawler/daily.test.js
+++ test/controllers/crawler/daily.test.js
@@ -7,14 +7,14 @@ describe("controllers/crawler/daily", function ()
 {
     describe("1.fetchStoryIndexes", function ()
     {
-        it("should fetch the story indexes of: 20130519", function (done)
+        it("should fetch the story indexes of date: 20130519", function (done)
         {
             var date = "20130519"
             var target = {
                 date: "20130519",
                 indexes: [401, 396, 395, 394, 390, 388]
             };
-            daily.fetchStoryIndexes("20130519", function (err, res)
+            daily.fetchStoryIndexes(date, function (err, res)
             {
                 should.not.exist(err);
                 res.date.should.equal(date);
@@ -23,11 +23,11 @@ describe("controllers/crawler/daily", function ()
             });
         });
         
-        it("should not fetch the story indexes of: 20130518", function (done)
+        it("should not fetch the story indexes of date: 20130518", function (done)
         {
             var date = "20130518"
             var target = {};
-            daily.fetchStoryIndexes("20130518", function (err, res)
+            daily.fetchStoryIndexes(date, function (err, res)
             {
                 should.not.exist(err);
                 res.should.deepEqual(target);
@@ -35,12 +35,32 @@ describe("controllers/crawler/daily", function ()
             });
         });
         
-        it("should not fetch the story indexes of: 20133030", function (done)
+        it("should not fetch the story indexes of date: 20133030", function (done)
         {
             var date = "20133030"
-            daily.fetchStoryIndexes("20133030", function (err, res)
+            daily.fetchStoryIndexes(date, function (err, res)
             {
                 should.exist(err);
+                done();
+            });
+        });
+    });
+    
+    describe("2.fetchStory", function ()
+    {
+        it("should fetch the story of id: 401", function (done)
+        {
+            var id = 401;
+            var title = "学英语才是正经事儿";
+            var imageSource = "人民教育出版社";
+            var shareURL = "http://daily.zhihu.com/story/401";
+            daily.fetchStory(id, function (err, res)
+            {
+                should.not.exist(err);
+                res.id.should.equal(id);
+                res.title.should.equal(title);
+                res.imageSource.should.equal(imageSource);
+                res.shareURL.should.equal(shareURL);
                 done();
             });
         });
