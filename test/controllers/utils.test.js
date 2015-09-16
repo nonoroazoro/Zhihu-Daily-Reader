1d081c090d3bc9aeb4fbb335f3b6e5d8cb2ac36b
nonoroazoro <xiaochao.k@gmail.com>
2015-09-16 12:04:00 +0800

    update

--- test/controllers/utils.test.js
+++ test/controllers/utils.test.js
@@ -7,43 +7,68 @@ describe("controllers/utils", function ()
 {
     describe("1.convertToZhihuDate", function ()
     {
-        var origin = "2015-09-15";
+        var date1 = "20150915"; // String: Zhihu Format
+        var date2 = "2015-09-15"; // String: Normal Format
+        var date3 = moment(date2).toDate(); // Date
         var target = "20150915";
-        var date1 = moment(origin); // String
-        var date2 = moment(origin).toDate(); // Date
-        it("should convert '" + origin + "' to: '" + target + "'", function (done)
+        it("should convert '" + date1 + "' to: '" + target + "'", function (done)
         {
             utils.convertToZhihuDate(date1).should.equal(target);
+            done();
+        });
+        
+        it("should convert '" + date2 + "' to: '" + target + "'", function (done)
+        {
             utils.convertToZhihuDate(date2).should.equal(target);
             done();
         });
         
-        origin = "2015-30-30";
-        it("should convert '" + origin + "' to: 'null'", function (done)
+        it("should convert '" + date3 + "' to: '" + target + "'", function (done)
         {
-            should(utils.convertToZhihuDate(origin)).equal(null);
+            utils.convertToZhihuDate(date3).should.equal(target);
+            done();
+        });
+        
+        var wrongDate = "2015-30-30";
+        it("should convert '" + wrongDate + "' to: 'null'", function (done)
+        {
+            should(utils.convertToZhihuDate(wrongDate)).equal(null);
             done();
         });
     });
     
     describe("2.nextZhihuDay", function ()
     {
-        var origin = "20150915";
+        var date = "20150915";
         var target = "20150916";
-        it("should convert '" + origin + "' to: '" + target + "'", function (done)
+        it("should convert '" + date + "' to: '" + target + "'", function (done)
         {
-            utils.nextZhihuDay(origin).should.equal(target);
+            utils.nextZhihuDay(date).should.equal(target);
+            done();
+        });
+        
+        var wrongDate = "20153030"
+        it("should convert '" + wrongDate + "' to: 'null'", function (done)
+        {
+            should(utils.nextZhihuDay(wrongDate)).equal(null);
             done();
         });
     });
     
     describe("3.prevZhihuDay", function ()
     {
-        var origin = "20150915";
+        var date = "20150915";
         var target = "20150914";
-        it("should convert '" + origin + "' to: '" + target + "'", function (done)
+        it("should convert '" + date + "' to: '" + target + "'", function (done)
         {
-            utils.prevZhihuDay(origin).should.equal(target);
+            utils.prevZhihuDay(date).should.equal(target);
+            done();
+        });
+        
+        var wrongDate = "20153030"
+        it("should convert '" + wrongDate + "' to: 'null'", function (done)
+        {
+            should(utils.prevZhihuDay(wrongDate)).equal(null);
             done();
         });
     });
