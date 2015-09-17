41f72dc6ab4c808f0137c703f106b894638c2774
nonoroazoro <xiaochao.k@gmail.com>
2015-09-17 17:17:52 +0800

    update

--- controllers/status.js
+++ controllers/status.js
@@ -3,20 +3,13 @@ var Status = require("../models/status");
 
 /**
  * 从数据库中查找指定用户的阅读状态。
- * @param  {Function} p_callback 回调函数：function(err, res)。
  * @param  {String} p_username   指定的用户名。
+ * @param  {Function} p_callback 回调函数：function(err, res)。
  */
-exports.findStatusByUsername = function (p_callback, p_username)
+exports.findStatusByUsername = function (p_username, p_callback)
 {
     if (_.isFunction(p_callback))
     {
-        if (_.isEmpty(_.trim(p_username)))
-        {
-            p_callback(new Error("p_username is an empty string."), null);
-        }
-        else
-        {
-            Status.findOne({ username: p_username }, p_callback);
-        }
+        Status.findOne({ username: p_username }, p_callback);
     }
 };
