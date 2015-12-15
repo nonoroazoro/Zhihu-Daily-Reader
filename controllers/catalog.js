2199ee7ea04fd215f383fadb9e85c4ae262f9466
nonoroazoro <xiaochao.k@gmail.com>
2015-12-15 13:37:41 +0800

    add catalog

---
+++ controllers/catalog.js
@@ -0,0 +1,52 @@
+﻿var _ = require("lodash");
+var Catalog = require("../models/catalog");
+
+/**
+ * 保存目录至数据库。如果已存在，则更新。
+ * @param {Object} p_catalog 目录。
+ * @param {Function(err, doc)} [p_callback]
+ */
+exports.saveCatalog = function (p_catalog, p_callback)
+{
+    if (_.isEmpty(p_catalog) || !_.isObject(p_catalog))
+    {
+        if (_.isFunction(p_callback))
+        {
+            p_callback(new Error("p_catalog must be a non-empty Object."))
+        }
+    }
+    else
+    {
+        Catalog.findOneAndUpdate(
+            { date: p_catalog.date },
+            p_catalog,
+            { new: true, upsert: true },
+            p_callback
+        );
+    }
+};
+
+/**
+ * 从数据库中查找指定日期的目录。
+ * @param {String} p_date 日期（知乎格式，例如："20130519"）。
+ * @param {Function(err, doc)} [p_callback]
+ */
+exports.findCatalogByDate = function (p_date, p_callback)
+{
+    if (_.isFunction(p_callback))
+    {
+        Catalog.findOne({ date: p_date }, p_callback);
+    }
+};
+
+/**
+ * 从数据库中查找满足条件的记录。
+ * @param {Object} p_conditions 指定的查询条件。
+ * @param {Object} [p_projection]
+ * @param {Object} [p_options]
+ * @param {Function(err, docs)} [p_callback]
+ */
+exports.query = function (p_conditions, p_projection, p_options, p_callback)
+{
+    Catalog.find(p_conditions, p_projection, p_options, p_callback);
+};
---
+++ models/catalog.js
@@ -0,0 +1,29 @@
+﻿var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+/**
+ * 存储知乎日报的目录（以天为单位）。
+ */
+var CatalogSchema = new Schema({
+    /**
+     * 日期（知乎格式，例如："20130519"）。
+     * @type {String}
+     */
+    date: String,
+    
+    /**
+     * 知乎日报 ID 列表。
+     * @type {Array}
+     */
+    ids: [Number],
+    
+    /**
+     * 是否已全部离线。
+     * @type {[String]}
+     */
+    cached: { type: Boolean, default: false }
+});
+
+CatalogSchema.index({ date: -1 }, { unique: true });
+
+module.exports = mongoose.model("Catalog", CatalogSchema);
