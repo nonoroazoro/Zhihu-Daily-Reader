95518181550c1c405168eca77f1a1fbb415c39cf
nonoroazoro <xiaochao.k@gmail.com>
2015-12-25 17:38:26 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 ﻿var gulp = require("gulp");
+var cached = require("gulp-cached");
 var jshint = require("gulp-jshint");
+var taskListing = require("gulp-task-listing");
 
-gulp.task("hint", function ()
+gulp.task("help", taskListing.withFilters(null, "help|default"));
+
+gulp.task("jshint", function ()
 {
-    gulp.src([
+    return gulp.src([
         "**/*.js",
         "!node_modules/**",
         "!./public/assets/libs/**",
         "!./public/assets/zdr/build/**"])
+    .pipe(cached("jshint"))
     .pipe(jshint())
-    .pipe(jshint.reporter());
+    .pipe(jshint.reporter("default"));
 });
 
-gulp.task("default", ["hint"] , function ()
+gulp.task("watch", function ()
+{
+    gulp.watch([
+        "**/*.js",
+        "!node_modules/**",
+        "!./public/assets/libs/**",
+        "!./public/assets/zdr/build/**"], ["jshint"]);
+});
+
+gulp.task("default", ["jshint"] , function ()
 {
 });
