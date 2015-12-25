170b694ba9c24315b8710a74fc148b71156eb8ea
nonoroazoro <xiaochao.k@gmail.com>
2015-12-25 15:20:15 +0800

    add gulp & jshint

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+﻿var gulp = require("gulp");
+var jshint = require("gulp-jshint");
+
+gulp.task("hint", function ()
+{
+    gulp.src([
+        "**/*.js",
+        "!node_modules/**",
+        "!./public/assets/libs/**",
+        "!./public/assets/zdr/build/**"])
+    .pipe(jshint())
+    .pipe(jshint.reporter());
+});
+
+gulp.task("default", ["hint"] , function ()
+{
+});
