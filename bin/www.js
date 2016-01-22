a2d55726667c66c4a61127b7105729e4bc304a5f
nonoroazoro <xiaochao.k@gmail.com>
2016-01-23 01:46:39 +0800

    update

--- bin/www.js
+++ bin/www.js
@@ -1,4 +1,4 @@
 ﻿var config = require("config");
 require("../app").listen(config.port);
 
-console.log("Application Server started on port: " + config.port);
+console.log("\nApplication Server started on port: " + config.port);
