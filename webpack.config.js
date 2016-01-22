6c2cab5d26ab5ae72ba37767c0b535c06936c81c
nonoroazoro <xiaochao.k@gmail.com>
2016-01-22 14:45:12 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ var webpack = require("webpack");
 var AssetsPlugin = require("assets-webpack-plugin");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var srcPath = path.resolve(__dirname, "./public/assets/zdr/");
-var buildPath = path.resolve(srcPath, "./build/");
+var srcPath = path.resolve(__dirname, "./client/zdr/");
+var buildPath = path.resolve(__dirname, "./public/assets/");
 
 var isDev = (process.env.NODE_ENV !== "production");
 if (isDev)
@@ -17,46 +17,64 @@ if (isDev)
 }
 
 var plugins = [
-    new ExtractTextPlugin("[name].css"),
-    //new webpack.optimize.CommonsChunkPlugin("vendors", "[name].js", Infinity),
-    new webpack.optimize.OccurenceOrderPlugin(),
-];
-
-if (!isDev)
-{
-    plugins.push(new AssetsPlugin(
+    new AssetsPlugin(
     {
         filename: "assets.json",
         path: buildPath,
         prettyPrint: true
+    }),
+    new ExtractTextPlugin("res/[name].css"),
+    new webpack.optimize.CommonsChunkPlugin(
+    {
+        names: "vendors",
+        filename: "vendors.js",
+        minChunks: Infinity
+    }),
+    new webpack.ProvidePlugin(
+    {
+        "$": "jquery",
+        "jQuery": "jquery",
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+];
+
+if (isDev)
+{
+    plugins.push(new webpack.SourceMapDevToolPlugin(
+    {
+        filename: "[file].map",
+        exclude: ["vendors.js"],
+        columns: false,
+        module: false
     }));
 }
 
 module.exports = {
-    devtool: isDev ? "inline-source-map" : null,
     entry:
     {
         zdr: srcPath,
-        error: path.resolve(srcPath, "./common/error_404")
+        error: path.resolve(srcPath, "./common/error_404"),
+        vendors: [
+            "jquery",
+            "react",
+            "react-addons-update",
+            "react-addons-pure-render-mixin",
+            "bootstrap",
+            "lodash",
+            "moment",
+            "mousetrap"
+        ]
     },
     output:
     {
-        path: isDev ? buildPath : path.join(buildPath, "/assets/[hash]/"),
-        publicPath: "/assets/[hash]/",
+        path: isDev ? buildPath : path.join(buildPath, "/[hash:8]/"),
+        publicPath: isDev ? "/assets/" : "/assets/[hash:8]/",
         filename: "[name].js",
         chunkFilename: "[id].js",
     },
     resolve:
     {
         extensions: ["", ".js", ".jsx"]
-    },
-    externals:
-    {
-        "react": "React",
-        "jquery": "jQuery",
-        "lodash": "_",
-        "moment": "moment",
-        "mousetrap": "Mousetrap"
     },
     module:
     {
