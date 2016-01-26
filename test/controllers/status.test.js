b2db65a56ffc08086e4e78a9ef7f9bd5a4d0448a
nonoroazoro <xiaochao.k@gmail.com>
2016-01-26 19:20:27 +0800

    remove .only

--- test/controllers/status.test.js
+++ test/controllers/status.test.js
@@ -4,7 +4,7 @@ import Status from "../../models/status";
 import StatusController from "../../controllers/status";
 import dbhelper from "../../controllers/dbhelper";
 
-describe.only("controllers/status", function ()
+describe("controllers/status", function ()
 {
     before(function (done)
     {
