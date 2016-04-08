4be0c77d43c851244efb8f7cf0451d874fe532ee
nonoroazoro <xiaochao.k@gmail.com>
2016-04-08 19:10:08 +0800

    render elements to #MainContainer

--- client/zdr/index.jsx
+++ client/zdr/index.jsx
@@ -1,14 +1,14 @@
 ﻿import "./res/index.less";
 
-import React from "react";
-import ReactDOM from "react-dom";
-import Navbar from "./components/Navbar";
+import React     from "react";
+import ReactDOM  from "react-dom";
+import Navbar    from "./components/Navbar";
 import DailyPage from "./daily/DailyPage";
 
 ReactDOM.render(
-    <div className="MainContainer container-fluid">
+    <div>
         <Navbar />
         <DailyPage />
     </div>,
-    document.body
+    document.getElementById("MainContainer")
 );
