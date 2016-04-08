da3dc4e3ec48cc295f67b4731d3b8b695d07fef4
nonoroazoro <xiaochao.k@gmail.com>
2016-04-08 19:27:12 +0800

    es6 rewrite

--- client/zdr/components/Navbar.jsx
+++ client/zdr/components/Navbar.jsx
@@ -1,91 +1,69 @@
-﻿require("./res/Navbar.less");
+﻿import "./res/Navbar.less";
 
-var React = require("react");
+import React from "react";
+import logo  from "../common/res/img/zhihu_daily_logo_blue.png";
 
 /**
  * 头部。
  */
-var NavbarHeader = React.createClass(
+class NavbarHeader extends React.Component
 {
-    getDefaultProps: function()
+    render()
     {
-        return {
-            target: ".navbar-collapse",
-        };
-    },
-
-    render: function ()
-    {
-        var navbarHeader =
+        return (
             <div className="navbar-header">
                 <button type="button"
                         className="navbar-toggle collapsed"
                         data-toggle="collapse"
-                        data-target={this.props.target}>
+                        data-target="#NavbarContent">
                     <span className="sr-only">导航</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
+                    <span className="icon-bar" />
+                    <span className="icon-bar" />
+                    <span className="icon-bar" />
                 </button>
                 <a className="navbar-brand" href="/">
-                    <img alt="知乎日报" src={require("../common/res/img/zhihu_daily_logo_blue.png")} />
+                    <img alt="知乎日报" src={logo} />
                 </a>
-            </div>;
-        return navbarHeader;
+            </div>
+        );
     }
-});
+}
 
 /**
  * 菜单。
  */
-var NavbarContent = React.createClass(
+class NavbarContent extends React.Component
 {
-    getDefaultProps: function()
+    render()
     {
-        return {
-            id: "NavbarContent",
-        };
-    },
-
-    render: function ()
-    {
-        var navbarContent =
-            <div id={this.props.id}
-                 className="navbar-collapse collapse">
+        return (
+            <div
+                id="NavbarContent"
+                className="navbar-collapse collapse">
                 <ul className="nav navbar-nav navbar-right">
                     <li className="active"><a href="#">日报</a></li>
                     <li className="disabled"><a href="#">专栏</a></li>
                     <li className="disabled"><a href="#">关于</a></li>
                 </ul>
-            </div>;
-        return navbarContent;
+            </div>
+        );
     }
-});
+}
 
 /**
  * 导航栏。
  */
-var Navbar = React.createClass(
+export default class Navbar extends React.Component
 {
-    getDefaultProps: function()
+    render()
     {
-        return {
-            id: "Navbar",
-        };
-    },
-
-    render: function ()
-    {
-        var navbar =
-            <nav id={this.props.id}
-                 className="Navbar navbar navbar-default navbar-fixed-top">
-                <div className="container">
-                    <NavbarHeader target="#NavbarContent" />
+        return (
+            <nav className="Navbar navbar navbar-default navbar-fixed-top">
+                <div className="container-fluid">
+                    <NavbarHeader />
                     <NavbarContent />
                 </div>
-            </nav>;
-        return navbar;
+            </nav>
+        );
     }
-});
-
-module.exports = Navbar;
+}
