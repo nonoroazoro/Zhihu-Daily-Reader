698b936e800b00634c60c9c07f48ecd6f2b12df0
nonoroazoro <xiaochao.k@gmail.com>
2016-06-06 16:47:25 +0800

    fix a bug

--- client/zdr/daily/DailyPage.jsx
+++ client/zdr/daily/DailyPage.jsx
@@ -187,7 +187,7 @@ export default class DailyPage extends React.Component
     */
     _addKeyboardShortcuts()
     {
-        Mousetrap.bind(["esc", "escape"], () =>
+        Mousetrap.bind(["esc"], () =>
         {
             this._closeArticleView();
             this._closeShortcutsView();
@@ -419,7 +419,10 @@ export default class DailyPage extends React.Component
      */
     _openShortcutsView()
     {
-        this._$ShortcutsView.modal();
+        if (!this._$ShortcutsView.is(":visible"))
+        {
+            this._$ShortcutsView.modal();
+        }
     }
 
     /**
@@ -427,7 +430,10 @@ export default class DailyPage extends React.Component
      */
     _closeShortcutsView()
     {
-        this._$ShortcutsView.modal("hide");
+        if (this._$ShortcutsView.is(":visible"))
+        {
+            this._$ShortcutsView.modal("hide");
+        }
     }
 
     /**
