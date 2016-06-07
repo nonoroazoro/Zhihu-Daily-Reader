4175620578000c2c8dc8cb55e353242fe28deb6a
nonoroazoro <xiaochao.k@gmail.com>
2016-06-07 13:55:28 +0800

    update for es6

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,11 +1,11 @@
-﻿var path = require("path");
-var webpack = require("webpack");
-var AssetsPlugin = require("assets-webpack-plugin");
-var CleanWebpackPlugin = require("clean-webpack-plugin");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+﻿const path = require("path");
+const webpack = require("webpack");
+const AssetsPlugin = require("assets-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var mainPath = path.resolve(__dirname, "./client/");
-var buildPath = path.resolve(__dirname, "./public/assets/");
+const mainPath = path.resolve(__dirname, "./client/");
+const buildPath = path.resolve(__dirname, "./public/assets/");
 
 module.exports = {
     context: mainPath,
@@ -14,7 +14,7 @@ module.exports = {
         vendor: [
             "jquery",
             "bootstrap",
-            
+
             // to reduce bundle file size.
             "lodash/map",
             "lodash/isDate",
@@ -82,23 +82,26 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin([buildPath]),
         new AssetsPlugin(
-        {
-            filename: "assets.json",
-            path: buildPath,
-            prettyPrint: true
-        }),
+            {
+                filename: "assets.json",
+                path: buildPath,
+                prettyPrint: true
+            }
+        ),
         new webpack.optimize.CommonsChunkPlugin(
-        {
-            name: "vendor",
-            filename: "vendor.js",
-            minChunks: Infinity
-        }),
+            {
+                name: "vendor",
+                filename: "vendor.js",
+                minChunks: Infinity
+            }
+        ),
         new webpack.optimize.DedupePlugin(),
         new ExtractTextPlugin("res/[name].css"),
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.ProvidePlugin(
-        {
-            "jQuery": "jquery",
-        })
+            {
+                "jQuery": "jquery",
+            }
+        )
     ]
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-﻿var webpack = require("webpack");
-var config = require("./webpack.base.config");
+﻿const webpack = require("webpack");
+const config = require("./webpack.base.config");
 
 config.module.preLoaders =
 [
