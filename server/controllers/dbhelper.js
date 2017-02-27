03877f62be643e5bda6db4a08e9c4088fb068428
nonoroazoro <xiaochao.k@gmail.com>
2017-02-27 20:09:12 +0800

    use openSet

--- server/controllers/dbhelper.js
+++ server/controllers/dbhelper.js
@@ -45,7 +45,8 @@ module.exports.start = function (p_callback)
 module.exports.connect = function (p_callback)
 {
     const connection = process.env.MONGODB_CONNECTION ? `mongodb://${process.env.MONGODB_CONNECTION}` : config.db;
-    mongoose.connect(
+    var db = mongoose.createConnection();
+    db.openSet(
         connection,
         {
             server: {
