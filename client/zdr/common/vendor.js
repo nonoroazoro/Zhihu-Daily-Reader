e07863c30fe1aa5d01169828d1036fb15491ce33
nonoroazoro <xiaochao.k@gmail.com>
2017-03-10 13:23:04 +0800

    add react-router

--- client/zdr/common/vendor.js
+++ client/zdr/common/vendor.js
@@ -2,11 +2,12 @@
  * Import all of the vendors here.
  */
 
-import "jquery";
-import "bootstrap";
 import "bootstrap/less/bootstrap.less";
 
-// Import sub-modules to reduce bundle file size.
+import "jquery";
+import "bootstrap";
+
+// Lodash. Import sub-modules to reduce bundle file size.
 import "lodash/map";
 import "lodash/isDate";
 import "lodash/isEmpty";
@@ -16,7 +17,8 @@ import "lodash/trim";
 
 import "moment";
 import "mousetrap";
-import "classnames";
 import "react";
 import "react-dom";
-import "react-addons-update";
+import "react-router";
+import "classnames";
+import "immutability-helper";
