46e2cc419203a063ff39ad848e478972850e46c9
nonoroazoro <xiaochao.k@gmail.com>
2017-03-10 20:31:05 +0800

    set retries

--- server/auth/session.js
+++ server/auth/session.js
@@ -9,5 +9,5 @@ module.exports = session({
     resave: false,
     saveUninitialized: false,
     secret: "e5d5033fa9e11b54627a2b42b409a0ec",
-    store: new FileStore({ path: "./.sessions" })
+    store: new FileStore({ path: "./.sessions", retries: 2 })
 });
