380e64f6374b024a998e15d9c851a891f666140f
nonoroazoro <xiaochao.k@gmail.com>
2017-04-14 16:11:54 +0800

    disable verbose logging

--- server/auth/session.js
+++ server/auth/session.js
@@ -9,5 +9,11 @@ module.exports = session({
     resave: false,
     saveUninitialized: false,
     secret: "e5d5033fa9e11b54627a2b42b409a0ec",
-    store: new FileStore({ path: "./.sessions", retries: 2 })
+    store: new FileStore(
+        {
+            path: "./.sessions",
+            retries: 2,
+            logFn: () => { }
+        }
+    )
 });
