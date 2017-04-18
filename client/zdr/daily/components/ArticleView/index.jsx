8fd5652ced7ba52df4c56631f8ffb0dcb0dfee59
nonoroazoro <xiaochao.k@gmail.com>
2017-04-18 16:41:37 +0800

    performance tweak

--- client/zdr/daily/components/ArticleView/index.jsx
+++ client/zdr/daily/components/ArticleView/index.jsx
@@ -1,4 +1,4 @@
-﻿import React, { PropTypes } from "react";
+﻿import React, { Component, PropTypes } from "react";
 
 import ArticleBody from "./ArticleBody";
 import ArticleHeader from "./ArticleHeader";
@@ -6,73 +6,87 @@ import ArticleControl from "./ArticleControl";
 
 import "./index.less";
 
-const ArticleView = ({ id, story, onPrevClick, onNextClick }) =>
+/**
+ * 知乎日报文章内容。
+ *
+ * @export
+ * @class ArticleView
+ * @extends {Component}
+ */
+export default class ArticleView extends Component
 {
-    let header = null;
-    let body = null;
-    if (story)
+    static propTypes = {
+        id: PropTypes.string,
+        story: PropTypes.shape({
+            backgrounds: PropTypes.arrayOf(
+                PropTypes.shape({
+                    href: PropTypes.string,
+                    text: PropTypes.string,
+                    title: PropTypes.string
+                })
+            ),
+            contents: PropTypes.arrayOf(
+                PropTypes.shape({
+                    answers: PropTypes.arrayOf(
+                        PropTypes.shape({
+                            avatar: PropTypes.string,
+                            bio: PropTypes.string,
+                            name: PropTypes.string,
+                            content: PropTypes.string
+                        })
+                    ),
+                    link: PropTypes.shape({
+                        href: PropTypes.string,
+                        text: PropTypes.string
+                    }),
+                    title: PropTypes.string
+                })
+            ),
+            image: PropTypes.string,
+            imageSource: PropTypes.string,
+            shareURL: PropTypes.string,
+            title: PropTypes.string
+        }),
+        onPrevClick: PropTypes.func,
+        onNextClick: PropTypes.func
+    };
+
+    static defaultProps = {
+        id: "ArticleView",
+        story: null,
+        onPrevClick: () => { },
+        onNextClick: () => { }
+    };
+
+    shouldComponentUpdate(p_nextProps, p_nextState)
     {
-        header = <ArticleHeader story={story} />;
-        if (story.contents)
-        {
-            body = <ArticleBody contents={story.contents} />;
-        }
+        return (p_nextProps.id !== this.props.id) || (p_nextProps.story !== this.props.story);
     }
 
-    return (
-        <div id={id} className="ArticleView modal fade">
-            <div className="modal-dialog modal-lg">
-                <div className="modal-content">
-                    {header}
-                    {body}
+    render()
+    {
+        const { id, story, onPrevClick, onNextClick } = this.props;
+        let header = null;
+        let body = null;
+        if (story)
+        {
+            header = <ArticleHeader story={story} />;
+            if (story.contents)
+            {
+                body = <ArticleBody contents={story.contents} />;
+            }
+        }
+
+        return (
+            <div id={id} className="ArticleView modal fade">
+                <div className="modal-dialog modal-lg">
+                    <div className="modal-content">
+                        {header}
+                        {body}
+                    </div>
+                    <ArticleControl onPrevClick={onPrevClick} onNextClick={onNextClick} />
                 </div>
-                <ArticleControl onPrevClick={onPrevClick} onNextClick={onNextClick} />
             </div>
-        </div>
-    );
-};
-
-ArticleView.propTypes = {
-    id: PropTypes.string,
-    story: PropTypes.shape({
-        backgrounds: PropTypes.arrayOf(
-            PropTypes.shape({
-                href: PropTypes.string,
-                text: PropTypes.string,
-                title: PropTypes.string
-            })
-        ),
-        contents: PropTypes.arrayOf(
-            PropTypes.shape({
-                answers: PropTypes.arrayOf(
-                    PropTypes.shape({
-                        avatar: PropTypes.string,
-                        bio: PropTypes.string,
-                        name: PropTypes.string,
-                        content: PropTypes.string
-                    })
-                ),
-                link: PropTypes.shape({
-                    href: PropTypes.string,
-                    text: PropTypes.string
-                }),
-                title: PropTypes.string
-            })
-        ),
-        image: PropTypes.string,
-        imageSource: PropTypes.string,
-        shareURL: PropTypes.string,
-        title: PropTypes.string
-    }),
-    onPrevClick: PropTypes.func,
-    onNextClick: PropTypes.func
-};
-
-ArticleView.defaultProps = {
-    id: "ArticleView",
-    story: null,
-    onPrevClick: () => { },
-    onNextClick: () => { }
-};
-
-export default ArticleView;
+        );
+    }
+}
