aecaf78516e1e3444a5071fc941d3ee2269c2964
nonoroazoro <xiaochao.k@gmail.com>
2017-04-18 16:25:11 +0800

    performance tweak

--- client/zdr/daily/components/ArticleView/ArticleControl.jsx
+++ client/zdr/daily/components/ArticleView/ArticleControl.jsx
@@ -1,42 +1,49 @@
-import React, { PropTypes } from "react";
+import React, { Component, PropTypes } from "react";
 
 /**
  * 两侧控制器。
  */
-const ArticleControl = ({ onPrevClick, onNextClick }) =>
+export default class ArticleControl extends Component
 {
-    return (
-        <div className="ArticleControl">
-            <a
-                className="left carousel-control"
-                href="javascript:;"
-                onClick={onPrevClick}
-                role="button"
-            >
-                <span className="glyphicon glyphicon-chevron-left" />
-                <span className="sr-only">上一篇</span>
-            </a>
-            <a
-                className="right carousel-control"
-                href="javascript:;"
-                onClick={onNextClick}
-                role="button"
-            >
-                <span className="glyphicon glyphicon-chevron-right" />
-                <span className="sr-only">下一篇</span>
-            </a>
-        </div>
-    );
-};
+    static propTypes = {
+        onPrevClick: PropTypes.func,
+        onNextClick: PropTypes.func
+    };
 
-ArticleControl.propTypes = {
-    onPrevClick: PropTypes.func,
-    onNextClick: PropTypes.func
-};
+    static defaultProps = {
+        onPrevClick: () => { },
+        onNextClick: () => { }
+    };
 
-ArticleControl.defaultProps = {
-    onPrevClick: () => { },
-    onNextClick: () => { }
-};
+    shouldComponentUpdate(p_nextProps, p_nextState)
+    {
+        return false;
+    }
 
-export default ArticleControl;
+    render()
+    {
+        const { onPrevClick, onNextClick } = this.props;
+        return (
+            <div className="ArticleControl">
+                <a
+                    className="left carousel-control"
+                    href="javascript:;"
+                    onClick={onPrevClick}
+                    role="button"
+                >
+                    <span className="glyphicon glyphicon-chevron-left" />
+                    <span className="sr-only">上一篇</span>
+                </a>
+                <a
+                    className="right carousel-control"
+                    href="javascript:;"
+                    onClick={onNextClick}
+                    role="button"
+                >
+                    <span className="glyphicon glyphicon-chevron-right" />
+                    <span className="sr-only">下一篇</span>
+                </a>
+            </div>
+        );
+    }
+}
